b5677df4c21cab39c4277c2c5c14011d2ca7bd8d
m-m-black <morganblack1988@gmail.com>
2021-01-16 18:10:03 +1100

    Added basename for Router

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 class App extends React.Component {
 	render() {
 		return (
-				<Router basename="/portfolio/">
+				<Router basename="/">
 					<Nav />
 						<Switch>
 							<Route path="/" exact component={Home} />
