afaa4567f176ff9e607ab0ad3d1ee92d9f21de4c
m-m-black <morganblack1988@gmail.com>
2021-01-19 15:42:49 +1100

    Added sketch descriptions

--- src/Home.js
+++ src/Home.js
@@ -11,21 +11,21 @@ class Home extends React.Component {
 				<SketchPreview 
 					url="https://m-m-black.github.io/circles/"
 					title="Circles"
-					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+					description="Circles pulsate to create a musical meditation. The larger the circle, the slower the pulsation. Pitch is determined by the position of the circle. Inspired by the phase-based compositions of Steve Reich."
 					image={circles}
 					alignment="left"
 				/>
 				<SketchPreview 
 					url="https://m-m-black.github.io/shapes/"
 					title="Shapes"
-					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+					description="Draw a shape to build a melody. Draw multiple shapes to create a forest of sound. Listen as the melodies interact, weaving through each other in surprising ways. Inspired by the music of Laurie Spiegel."
 					image={shapes}
 					alignment="right"
 				/>
 				<SketchPreview 
 					url="https://m-m-black.github.io/gridseq/"
 					title="GridSeq"
-					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+					description="A musical sequencer that operates in 2 dimensions. Rhythms propagate outwards (up, down, left, right) from active cells. Sounds are triggered from the intersections of these cells. Unpredictable rhythms may occur."
 					image={gridseq}
 					alignment="left"
 				/>
