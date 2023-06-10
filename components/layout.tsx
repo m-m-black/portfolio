a7deb795432008f3493579d4fca11816412feb00
m-m-black <16482752+m-m-black@users.noreply.github.com>
2023-06-10 13:58:07 +1000

    Change viewport-fit to cover

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,10 @@ const Layout = ({ children }) => {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0 viewport-fit=cover"
+        />
       </Head>
       <Navbar />
       <main className={styles.main}>{children}</main>
