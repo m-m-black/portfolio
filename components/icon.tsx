669f4f45637e711a81cc5e3eefebf989651b0302
m-m-black <16482752+m-m-black@users.noreply.github.com>
2026-05-20 18:47:27 +1000

    Type fix

--- components/icon.tsx
+++ components/icon.tsx
@@ -1,18 +1,13 @@
-import type { ComponentType } from "react";
-
-type RadixIconComponent = ComponentType<{ className?: string }>;
+import type { ElementType } from "react";
 
 interface IconProps {
-  icon: RadixIconComponent;
+  icon: ElementType;
   size?: number;
   className?: string;
 }
 
 const Icon = ({ icon: RadixIcon, size = 20, className }: IconProps) => (
-  <RadixIcon
-    className={className}
-    style={{ width: size, height: size }}
-  />
+  <RadixIcon width={size} height={size} className={className} />
 );
 
 export default Icon;
