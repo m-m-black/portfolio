2177125a0f79e35acccd7d1c7ca1c9e778691ae8
m-m-black <16482752+m-m-black@users.noreply.github.com>
2026-05-22 19:26:06 +1000

    Add data loading

---
+++ components/ExcavatingLoader.tsx
@@ -0,0 +1,10 @@
+const ExcavatingLoader = () => (
+  <p className="text-text-muted text-sm tracking-widest uppercase">
+    excavating
+    <span className="animate-ellipsis-1">.</span>
+    <span className="animate-ellipsis-2">.</span>
+    <span className="animate-ellipsis-3">.</span>
+  </p>
+)
+
+export default ExcavatingLoader
